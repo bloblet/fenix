b6393d4df5cb8b4137e9b4fca4f8cc629acef8d8
piecubed <40523150+piecubed@users.noreply.github.com>
2020-09-06 20:40:17 -0400

    yay

---
+++ api/grpc.api.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"context"
+	pb "fenix/proto/6.0.1"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type GRPCApi struct {
+	s *grpc.Server
+}
+
+func (api *GRPCApi) Serve() {
+	lis, err := net.Listen("tcp", "localhost:3000")
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	api.s = grpc.NewServer()
+	pb.RegisterUsersService(api.s, &pb.UsersService{Get: api.Get})
+
+	if err := api.s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+func (api *GRPCApi) Get(ctx context.Context, in *pb.Authenticate) (*pb.User, error) {
+	log.Printf("Received: %v", in.GetID())
+	return &pb.User{ID: in.GetID()}, nil
+}
