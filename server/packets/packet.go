800aba87852e9b6b8474806a2103006404bddb0d
piecubed <40523150+piecubed@users.noreply.github.com>
2020-09-19 12:02:28 -0400

    Delete unused code.

--- server/packets/packet.go
+++
@@ -1,8 +0,0 @@
-package packets
-
-// A Packet is a base interface for all supported packets, that works with all supported versions.
-type Packet interface {
-	CopyToNewPacket(Packet)
-	FromJSON(map[string]interface{}) (Packet, error)
-	SetID(id interface{})
-}
