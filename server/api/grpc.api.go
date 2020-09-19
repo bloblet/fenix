0917c705c5a518f34e4df9bd33f0ca30d5d6a845
piecubed <40523150+piecubed@users.noreply.github.com>
2020-09-18 20:35:41 -0400

    Bug fix: Remove obsolete test

--- server/api/grpc.api.go
+++ server/api/grpc.api.go
@@ -14,20 +14,19 @@ type GRPCApi struct {
 }
 
 func (api *GRPCApi) Serve() {
-	lis, err := net.Listen("tcp", "0.0.0.0:4000")
+	api.s = grpc.NewServer()
+	pb.RegisterUsersService(api.s, &pb.UsersService{Get: api.get})
 
+	lis, err := net.Listen("tcp", "0.0.0.0:4000")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
-	api.s = grpc.NewServer()
-	pb.RegisterUsersService(api.s, &pb.UsersService{Get: api.Get})
-
+	
 	if err := api.s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-func (api *GRPCApi) Get(_ context.Context, in *pb.Authenticate) (*pb.User, error) {
+func (api *GRPCApi) get(_ context.Context, in *pb.Authenticate) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetID())
 	return &pb.User{ID: in.GetID() + in.GetToken()}, nil
 }
--- server/api_test.go
+++ server/api_test.go
@@ -1,136 +1,136 @@
-package main
+// package main
 
-import (
-	"bytes"
-	"context"
-	"encoding/json"
-	api "bloblet/fenix/api"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"testing"
+// import (
+// 	"bytes"
+// 	"context"
+// 	"encoding/json"
+// 	api "bloblet/fenix/api"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"os"
+// 	"testing"
 
-	"github.com/google/uuid"
-)
+// 	"github.com/google/uuid"
+// )
 
-const endpoint = "http://localhost:8080"
+// const endpoint = "http://localhost:8080"
 
-func setupTestCase(t *testing.T) func(bool) {
-	testID, _ := uuid.NewUUID()
+// func setupTestCase(t *testing.T) func(bool) {
+// 	testID, _ := uuid.NewUUID()
 
-	password, ok := os.LookupEnv("FENIX_PASS")
+// 	password, ok := os.LookupEnv("FENIX_PASS")
 
-	if !ok {
-		t.Fatal("No FENIX_PASS env var set!")
-	}
+// 	if !ok {
+// 		t.Fatal("No FENIX_PASS env var set!")
+// 	}
 
-	a := api.NewAPI("testing", password, testID.String())
+// 	a := api.NewAPI("testing", password, testID.String())
 
-	a.Serve(true)
+// 	a.Serve(true)
 
-	return func(ok bool) {
-		cli, _ := a.UserDatabase.Database()
+// 	return func(ok bool) {
+// 		cli, _ := a.UserDatabase.Database()
 
-		cli.Delete(context.Background(), "/testing/"+testID.String()+"*")
+// 		cli.Delete(context.Background(), "/testing/"+testID.String()+"*")
 
-		cli.Close()
+// 		cli.Close()
 
-		if ok {
-			os.Exit(0)
-		}
-		os.Exit(1)
-	}
-}
+// 		if ok {
+// 			os.Exit(0)
+// 		}
+// 		os.Exit(1)
+// 	}
+// }
 
-func CreateUser(bodyJSON, email, password string) (int, map[string]interface{}, error) {
-	b := bytes.Buffer{}
+// func CreateUser(bodyJSON, email, password string) (int, map[string]interface{}, error) {
+// 	b := bytes.Buffer{}
 
-	b.Write([]byte(bodyJSON))
+// 	b.Write([]byte(bodyJSON))
 
-	req, err := http.NewRequest("POST", endpoint+"/6.0.1/create", &b)
+// 	req, err := http.NewRequest("POST", endpoint+"/6.0.1/create", &b)
 
-	if err != nil {
-		return 0, nil, err
-	}
+// 	if err != nil {
+// 		return 0, nil, err
+// 	}
 
-	req.Header.Set("Content-Type", "application/json")
-	req.SetBasicAuth(email, password)
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.SetBasicAuth(email, password)
 
-	client := &http.Client{}
+// 	client := &http.Client{}
 
-	res, err := client.Do(req)
+// 	res, err := client.Do(req)
 
-	if err != nil {
-		return 0, nil, err
-	}
+// 	if err != nil {
+// 		return 0, nil, err
+// 	}
 
-	var body map[string]interface{}
+// 	var body map[string]interface{}
 
-	bodyBytes, err := ioutil.ReadAll(res.Body)
+// 	bodyBytes, err := ioutil.ReadAll(res.Body)
 
-	if err != nil {
-		return 0, nil, err
-	}
+// 	if err != nil {
+// 		return 0, nil, err
+// 	}
 
-	err = json.Unmarshal(bodyBytes, &body)
+// 	err = json.Unmarshal(bodyBytes, &body)
 
-	if err != nil {
-		return 0, nil, err
-	}
+// 	if err != nil {
+// 		return 0, nil, err
+// 	}
 
-	return res.StatusCode, body, nil
-}
+// 	return res.StatusCode, body, nil
+// }
 
-type createCase struct {
-	name, email, password, json string
-	okResult                    bool
-}
+// type createCase struct {
+// 	name, email, password, json string
+// 	okResult                    bool
+// }
 
-func newCreateCase(json, email, password, name string, okResult bool) createCase {
-	c := createCase{}
-	c.name = name
-	c.json = json
-	c.email = email
-	c.password = password
-	c.okResult = okResult
+// func newCreateCase(json, email, password, name string, okResult bool) createCase {
+// 	c := createCase{}
+// 	c.name = name
+// 	c.json = json
+// 	c.email = email
+// 	c.password = password
+// 	c.okResult = okResult
 
-	return c
-}
+// 	return c
+// }
 
-func TestCreate(t *testing.T) {
-	teardownTestCase := setupTestCase(t)
-	var testCases [4]createCase
+// func TestCreate(t *testing.T) {
+// 	teardownTestCase := setupTestCase(t)
+// 	var testCases [4]createCase
 
-	testCases[0] = newCreateCase("{\"username\":\"Rick Astley\"}", "rick.astley@rickroll.com", "yay", "CreateUser", true)
-	testCases[1] = newCreateCase("{\"username\":\"Rick Astley\"}", "rick.astley@rickroll.com", "yay", "CreateDuplicateUser", false)
-	testCases[2] = newCreateCase("{\"username\":\"Rick Astley Likes To Rickroll People With Long Usernames That Are Over 32 Characters\"}", "astley.rick@rickroll.com", "yay", "CreateUserWithLongName", false)
-	testCases[3] = newCreateCase("invalid json", "invalid.astley@rickroll.com", "yay", "CreateUserWithInvalidJson", false)
+// 	testCases[0] = newCreateCase("{\"username\":\"Rick Astley\"}", "rick.astley@rickroll.com", "yay", "CreateUser", true)
+// 	testCases[1] = newCreateCase("{\"username\":\"Rick Astley\"}", "rick.astley@rickroll.com", "yay", "CreateDuplicateUser", false)
+// 	testCases[2] = newCreateCase("{\"username\":\"Rick Astley Likes To Rickroll People With Long Usernames That Are Over 32 Characters\"}", "astley.rick@rickroll.com", "yay", "CreateUserWithLongName", false)
+// 	testCases[3] = newCreateCase("invalid json", "invalid.astley@rickroll.com", "yay", "CreateUserWithInvalidJson", false)
 
-	for testIndex, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			statusCode, body, err := CreateUser(testCase.json, testCase.email, testCase.password)
-			// This should never return an error, since any errors we expect are just API errors.  If this happens, something is wrong.
-			if err != nil {
-				// Values are surrounded by zero width spaces (​) (Unicode U+200B)
-				t.Errorf("Failed on ​%v​.  Status code: ​%v​.  Body: ​%v​.  Error: ​%v​", testIndex, statusCode, body, err)
+// 	for testIndex, testCase := range testCases {
+// 		t.Run(testCase.name, func(t *testing.T) {
+// 			statusCode, body, err := CreateUser(testCase.json, testCase.email, testCase.password)
+// 			// This should never return an error, since any errors we expect are just API errors.  If this happens, something is wrong.
+// 			if err != nil {
+// 				// Values are surrounded by zero width spaces (​) (Unicode U+200B)
+// 				t.Errorf("Failed on ​%v​.  Status code: ​%v​.  Body: ​%v​.  Error: ​%v​", testIndex, statusCode, body, err)
 
-				return
-			}
+// 				return
+// 			}
 
-			// 201 Creates is the correct response for this.
-			if (statusCode == 201) == testCase.okResult {
-				t.Logf("%v ok", testIndex)
-			} else {
-				expected := "succeed"
-				if !testCase.okResult {
-					expected = "fail"
-				}
+// 			// 201 Creates is the correct response for this.
+// 			if (statusCode == 201) == testCase.okResult {
+// 				t.Logf("%v ok", testIndex)
+// 			} else {
+// 				expected := "succeed"
+// 				if !testCase.okResult {
+// 					expected = "fail"
+// 				}
 
-				t.Logf("Failed on ​%v​.  Status code: ​%v​.  Body: ​%v​.  Expected function to %v", testIndex, statusCode, body, expected)
-				t.Fail()
-			}
-		})
-	}
+// 				t.Logf("Failed on ​%v​.  Status code: ​%v​.  Body: ​%v​.  Expected function to %v", testIndex, statusCode, body, expected)
+// 				t.Fail()
+// 			}
+// 		})
+// 	}
 
-	teardownTestCase(!t.Failed())
-}
+// 	teardownTestCase(!t.Failed())
+// }
