d3022bb05b3dc936ea9fc5159095c1d811eec4b7
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2020-09-19 11:48:45 -0500

    Add protobufs for getting a client's username.

--- gen_proto.sh
+++ gen_proto.sh
@@ -6,4 +6,5 @@ protoc --go_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_s
        --go-grpc_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:. \
        --go_opt=paths=source_relative \
        --go-grpc_opt=paths=source_relative \
-       proto/6.0.1/user.proto
+       proto/6.0.1/user.proto \
+       proto/6.0.1/auth.proto
