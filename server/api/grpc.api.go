4db65c70933bdf37bcc8fc766e466cf2cc58fe95
piecubed <40523150+piecubed@users.noreply.github.com>
2020-09-20 14:31:47 -0400

    Add session tokens

--- server/api/grpc.api.go
+++ server/api/grpc.api.go
@@ -2,15 +2,29 @@ package api
 
 import (
 	"context"
-	pb "github.com/bloblet/fenix/proto/6.0.1"
+	"crypto/rand"
+	"encoding/base64"
 	"log"
+	mrand "math/rand"
 	"net"
+	"strconv"
+	"time"
 
+	pb "github.com/bloblet/fenix/proto/6.0.1"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+func generateToken(n int) (string, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
 type GRPCApi struct {
-	s *grpc.Server
+	s        *grpc.Server
+	sessions map[string]string
 }
 
 func (api *GRPCApi) Serve() {
@@ -22,11 +36,13 @@ func (api *GRPCApi) Serve() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	
+
 	if err := api.s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
+
+// DEPRECATED, pending removal
 func (api *GRPCApi) get(_ context.Context, in *pb.Authenticate) (*pb.User, error) {
 	log.Printf("Received: %v", in.GetID())
 	return &pb.User{ID: in.GetID() + in.GetToken()}, nil
@@ -36,6 +52,26 @@ func (api *GRPCApi) connect(_ context.Context, _ *pb.OpenHandshake) (*pb.FetchCl
 	return &pb.FetchClientAuth{}, nil
 }
 
+// gRPC doesn't have any way of identifying clients, other than client metadata.
+// To avoid cluttering all the protobuf requests with token parameters, and to avoid messy bidirectional stream workarounds,
+// Fenix uses session tokens in metadata.  Clients are expected to log in and then keep that session token in metadata, and renew 
+// when it expires.  If anyone has a better solution, open an issue.
 func (api *GRPCApi) login(_ context.Context, in *pb.ClientAuth) (*pb.AuthAck, error) {
-	return &pb.AuthAck{Username: in.GetUsername()}, nil
+	sessionToken, err := generateToken(16)
+	if err != nil {
+		return nil, err
+	}
+	psudeoUniqueUsername := in.GetUsername() + strconv.Itoa(mrand.Intn(1000))
+	api.sessions[sessionToken] = psudeoUniqueUsername
+
+	defer func() {
+		time.NewTimer(5 * time.Minute)
+		delete(api.sessions, sessionToken)
+	}()
+
+	return &pb.AuthAck{
+		Username: psudeoUniqueUsername, 
+		SessionToken: sessionToken, 
+		Expiry: timestamppb.New(time.Now().Add(5 * time.Minute)),
+		}, nil
 }
