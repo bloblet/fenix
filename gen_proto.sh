487ea8266084a8c4e238aca6b3d9a130394cb369
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2020-09-26 09:19:07 -0500

    Message Protobuf

--- gen_proto.sh
+++ gen_proto.sh
@@ -7,4 +7,5 @@ protoc --go_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_s
        --go_opt=paths=source_relative \
        --go-grpc_opt=paths=source_relative \
        proto/6.0.1/user.proto \
-       proto/6.0.1/auth.proto
+       proto/6.0.1/auth.proto \
+       proto/6.0.1/message.proto
