7a7b1801b9744f99a03f2bd01518d33007da944d
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2020-11-20 19:36:50 -0600

    Re-add proto generation script.

---
+++ gen_proto.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+go get github.com/golang/protobuf/protoc-gen-go
+go get google.golang.org/grpc/cmd/protoc-gen-go-grpc
+protoc --proto_path=./ \
+       --proto_path=/usr/include/ \
+       --go_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:. \
+       --go-grpc_out=Mgrpc/service_config/service_config.proto=/internal/proto/grpc_service_config:. \
+       --go_opt=paths=source_relative \
+       --go-grpc_opt=paths=source_relative \
+       proto/6.0.1/user.proto \
+       proto/6.0.1/auth.proto \
+       proto/6.0.1/message.proto
